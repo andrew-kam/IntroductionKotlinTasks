f9fd072eb09879f9daf013d8b973ef1409a04c3d
andrew-kam <andrewkamvik@gmail.com>
2023-04-17 22:37:40 +0300

    refactor

--- src/test/kotlin/ThirdTaskTest.kt
+++ src/test/kotlin/ThirdTaskTest.kt
@@ -1,15 +1,19 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
+
 class ThirdTaskTest {
 
     @Test
     fun testSquareAllElements() {
 
-        val baseList = listOf(13.31, 3.98, 12.0, 2.99, 9.0, null)
-        val result = 22.09
+        val baseList = listOf(13.31, 3.98, 12.0, 2.99, 9.0, null, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 1.0)
+        val result = 37.09
 
-        assertEquals(result.toString(), captureOutput { print("%.2f".format(chainOperation(baseList))) })
+        assertEquals(
+            message = "Sum is incorrect!",
+            actual = captureOutput { print("%.2f".format(chainOperation(baseList))) },
+            expected = result.toString()
+        )
     }
-
 }
