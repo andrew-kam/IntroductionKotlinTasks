82201530736f111d58b584470af8321dbb88af97
andrew-kam <andrewkamvik@gmail.com>
2023-04-17 15:21:03 +0300

    refactor

--- src/main/kotlin/FirstTask.kt
+++ src/main/kotlin/FirstTask.kt
@@ -1,16 +1,10 @@
-import kotlin.math.absoluteValue
-
-
 fun MutableList<Int>.squareAllElements() {
-
-    val minIntNumber = -2147483648
-    val maxValueElement = 46340
 
     this.forEachIndexed { index: Int, value: Int ->
 
         this[index] = value * value
 
-        if (value.absoluteValue > maxValueElement || value == minIntNumber) {
+        if (value.toLong() * value.toLong() > Int.MAX_VALUE) {
             println("Переполнение типа Int на элементе $value, индекс - [$index]")
         }
     }
