a2caed3091323c8174af6138f22c7ff80a4b9e0f
andrew-kam <andrewkamvik@gmail.com>
2023-04-17 18:14:18 +0300

    refactor

--- src/test/kotlin/Services.kt
+++ src/test/kotlin/Services.kt
@@ -2,10 +2,10 @@ import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
 
-fun captureOutput(block: () -> Unit): String {
+fun captureOutput(functionOutput: () -> Unit): String {
     val outPut = ByteArrayOutputStream()
     System.setOut(PrintStream(outPut))
-    block()
+    functionOutput()
     System.setOut(System.out)
     return outPut.toString().trim()
 }
