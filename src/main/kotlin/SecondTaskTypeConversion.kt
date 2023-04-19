540cb82db93b69a4c7130532ff80dad4d54b4fd0
andrew-kam <andrewkamvik@gmail.com>
2023-04-19 09:17:14 +0300

    format

--- src/main/kotlin/SecondTaskTypeConversion.kt
+++ src/main/kotlin/SecondTaskTypeConversion.kt
@@ -2,43 +2,38 @@ import java.time.LocalDate
 import java.time.Month
 
 
+private val TINKOFF_FOUNDATION_DATE = LocalDate.of(2006, Month.DECEMBER, 24)
+
 fun typeSelection(inputObject: Any?) {
 
     when (inputObject) {
 
-        is String -> {
+        is String ->
             println("Я получил String = '$inputObject', ее длина равна ${inputObject.length}")
-        }
 
-        is Int -> {
+        is Int ->
             println("Я получил Int = $inputObject, его квадрат равен ${inputObject * inputObject}")
-        }
 
-        is Double -> {
+        is Double ->
             println(
                 "Я получил Double = $inputObject, это число округляется до ${
                     "%.2f".format(inputObject).replace(Regex("\\.?0*$"), "")
                 }"
             )
-        }
 
-        is LocalDate -> {
-            val tinkoffFoundationDate = LocalDate.of(2006, Month.DECEMBER, 24)
+        is LocalDate ->
             println(
                 "Я получил LocalData = $inputObject," +
                         " эта дата ${
-                            if (inputObject < tinkoffFoundationDate) "меньше"
+                            if (inputObject < TINKOFF_FOUNDATION_DATE) "меньше"
                             else "больше или равна"
                         } чем дата основания Tinkoff"
             )
-        }
 
-        null -> {
+        null ->
             println("Объект равен null")
-        }
 
-        else -> {
+        else ->
             println("Мне этот тип неизвестен")
-        }
     }
 }
