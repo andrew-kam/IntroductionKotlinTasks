ac181886184facf67d265b3907c1f08082468f7d
andrew-kam <andrewkamvik@gmail.com>
2023-04-19 09:17:53 +0300

    add params

--- src/test/kotlin/SecondTaskTest.kt
+++ src/test/kotlin/SecondTaskTest.kt
@@ -1,90 +1,46 @@
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.LocalDate
 import java.util.logging.Handler
+import java.util.stream.Stream
 import kotlin.test.assertEquals
 
 
 class SecondTaskTest {
 
-    @Test
-    fun testTypeSelectionString() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection("Privet!") },
-            expected = "Я получил String = 'Privet!', ее длина равна 7"
-        )
+    companion object {
+        @JvmStatic
+        private fun typeSelectionProvider(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of("Privet!", "Я получил String = 'Privet!', ее длина равна 7"),
+                Arguments.of(234, "Я получил Int = 234, его квадрат равен 54756"),
+                Arguments.of(223.3943572, "Я получил Double = 223.3943572, это число округляется до 223.39"),
+                Arguments.of(731.0, "Я получил Double = 731.0, это число округляется до 731"),
+                Arguments.of(
+                    LocalDate.of(2006, 12, 23),
+                    "Я получил LocalData = 2006-12-23, эта дата меньше чем дата основания Tinkoff"
+                ),
+                Arguments.of(
+                    LocalDate.of(2006, 12, 24),
+                    "Я получил LocalData = 2006-12-24, эта дата больше или равна чем дата основания Tinkoff"
+                ),
+                Arguments.of(
+                    LocalDate.of(2006, 12, 25),
+                    "Я получил LocalData = 2006-12-25, эта дата больше или равна чем дата основания Tinkoff"
+                ),
+                Arguments.of(null, "Объект равен null"),
+                Arguments.of(Handler::class, "Мне этот тип неизвестен")
+            )
     }
 
-    @Test
-    fun testTypeSelectionInt() {
+    @ParameterizedTest
+    @MethodSource("typeSelectionProvider")
+    fun testTypeSelection(inputObject: Any?, expected: String) {
         assertEquals(
             message = "Output is wrong!",
-            actual = captureOutput { typeSelection(234) },
-            expected = "Я получил Int = 234, его квадрат равен 54756"
+            actual = captureOutput { typeSelection(inputObject) },
+            expected = expected
         )
     }
-
-    @Test
-    fun testTypeSelectionDouble() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(223.3943572) },
-            expected = "Я получил Double = 223.3943572, это число округляется до 223.39"
-        )
-    }
-
-    @Test
-    fun testTypeSelectionDoubleWithoutFraction() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(731.0) },
-            expected = "Я получил Double = 731.0, это число округляется до 731"
-        )
-    }
-
-    @Test
-    fun testTypeSelectionDateLess() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(LocalDate.of(2006, 12, 23)) },
-            expected = "Я получил LocalData = 2006-12-23, эта дата меньше чем дата основания Tinkoff"
-        )
-    }
-
-    @Test
-    fun testTypeSelectionDateMore() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(LocalDate.of(2006, 12, 25)) },
-            expected = "Я получил LocalData = 2006-12-25, эта дата больше или равна чем дата основания Tinkoff"
-        )
-    }
-
-    @Test
-    fun testTypeSelectionDateEqual() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(LocalDate.of(2006, 12, 24)) },
-            expected = "Я получил LocalData = 2006-12-24, эта дата больше или равна чем дата основания Tinkoff"
-        )
-    }
-
-    @Test
-    fun testTypeSelectionNull() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(null) },
-            expected = "Объект равен null"
-        )
-    }
-
-    @Test
-    fun testTypeSelectionUnknown() {
-        assertEquals(
-            message = "Output is wrong!",
-            actual = captureOutput { typeSelection(Handler::class) },
-            expected = "Мне этот тип неизвестен"
-        )
-    }
-
 }
