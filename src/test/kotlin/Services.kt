62aef1f3c9a017d0bff7f1866240bdeee21d8efc
andrew-kam <andrewkamvik@gmail.com>
2023-04-19 08:26:06 +0300

    format

--- src/test/kotlin/Services.kt
+++ src/test/kotlin/Services.kt
@@ -3,9 +3,12 @@ import java.io.PrintStream
 
 
 fun captureOutput(functionOutput: () -> Unit): String {
+
     val outPut = ByteArrayOutputStream()
+
     System.setOut(PrintStream(outPut))
     functionOutput()
     System.setOut(System.out)
+
     return outPut.toString().trim()
 }
