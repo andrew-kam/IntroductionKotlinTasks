f730e356d1b88de9f0ad134d2a6c68c725a71b16
andrew-kam <andrewkamvik@gmail.com>
2023-04-19 10:19:05 +0300

    refactor

--- src/main/kotlin/ThirdTaskChainOperations.kt
+++ src/main/kotlin/ThirdTaskChainOperations.kt
@@ -1,10 +1,8 @@
-fun chainOperation(list: List<Double?>): Double {
-    return list
-        .asSequence()
+fun chainOperation(list: List<Double?>): Double =
+    list.asSequence()
         .filterNotNull()
         .map { if (it.toInt() % 2 == 0) it * it else it / 2 }
         .filter { it <= 25 }
         .sortedDescending()
         .take(10)
         .sum()
-}
